7e5d131ba73292b9a2f5ba3349fbe0944009f55c
sactage <sactage@gmail.com>
2014-03-19 00:18:58 -0400

    update Holo plugin

--- plugins/holo.rb
+++ plugins/holo.rb
@@ -2,7 +2,7 @@ class Chatbot::Holo
   include Chatbot::Plugin
   match /^Holo, I love you/, :use_prefix => false
   match /^Holo, kill/, :use_prefix => false, :method => :poor_cod4
-  match /^\* (.*) gives Holo an apple/, :use_prefix => false, :method => :apple
+  match /^\*.* gives Holo an apple/, :use_prefix => false, :method => :apple
   def execute(captures, user)
     if user.name.eql? 'Sactage'
       @client.send_msg '/me snuggles up to Sactage'
