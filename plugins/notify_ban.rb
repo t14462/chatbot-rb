fa3225e96bfdbc7e73dbf7e56697baeedb3eaa19
sactage <sactage@gmail.com>
2014-03-24 08:51:31 -0400

    Make notify ban work! (note: bot edits not yet supported :( )

--- plugins/notify_ban.rb
+++ plugins/notify_ban.rb
@@ -5,7 +5,7 @@ class BanNotify
 
   BAN_TEMPLATE = 'User:URL/chatban'
   BAN_PAGE = 'Project:Chat/Bans'
-  REPLACE_TEMP = <<-repl
+  REPLACE_TEMP = <<-repl.gsub(/^\s+/, '')
   ==Temporary bans==
   {| class="wikitable sortable"
     !Username
@@ -20,20 +20,21 @@ class BanNotify
   def execute(data)
     seconds = data['attrs']['time'].to_i
     return if seconds == 0
+    pagetext = @client.api.get(BAN_PAGE)
     if seconds == 31536000000
-      text = '==Permabans==' + @client.api.get(BAN_PAGE).split('==Permabans==')[1]
-      @client.api.edit(BAN_PAGE, text.gsub(/\|\}/, <<-repl
+      text = '==Permabans==' + pagetext.split('==Permabans==')[1]
+      new_text = text.gsub /\|\}/, <<-repl.gsub(/^\s+/, '')
       |-
       |[[User:#{data['attrs']['kickedUserName']}|]]
       |#{Time.now.utc.strftime('%B %d, %Y')}
       |To be filled in by [[User:#{data['attrs']['moderatorName']}|]]
       |[[User:#{data['attrs']['moderatorName']}|]]
-      |-
+      |}
       repl
-      ), :bot => 1, :summary => "Adding ban for [[User:#{data['attrs']['kickedUserName']}|]]")
+      @client.api.edit(BAN_PAGE, pagetext.gsub(text, new_text), {:bot => 1, :summary => "Adding ban for [[User:#{data['attrs']['kickedUserName']}|]]"})
     else
       expiry = Time.at(Time.now.to_i + seconds).utc.strftime '%B %d, %Y'
-      replace = <<-repl
+      replace = <<-repl.gsub(/^\s+/, '')
       ==Temporary bans==
       {| class="wikitable sortable"
       !Username
@@ -54,8 +55,8 @@ class BanNotify
       page_text = @client.api.get(BAN_PAGE)
       puts page_text.include? REPLACE_TEMP
       page_text.gsub!(REPLACE_TEMP, replace)
-      @client.api.edit(BAN_PAGE, page_text, :summary => "Adding ban for [[User:#{data['attrs']['kickedUserName']}|]]", :bot => 1)
-      @client.api.edit('User_talk:' + data['attrs']['kickedUserName'], "{{subst:#{BAN_TEMPLATE}|#{data['attrs']['moderatorName']}|#{expiry}|#{data['attrs']['kickedUserName'].gsub(/ /, '_')}|#{Time.now.utc.strftime("%H:%M, %B %d, %Y (UTC)")}}}", :section => 'new')
+      @client.api.edit(BAN_PAGE, page_text, {:summary => "Adding ban for [[User:#{data['attrs']['kickedUserName']}|]]", :bot => 1})
+      @client.api.edit('User_talk:' + data['attrs']['kickedUserName'], "{{subst:#{BAN_TEMPLATE}|#{data['attrs']['moderatorName']}|#{expiry}|#{data['attrs']['kickedUserName'].gsub(/ /, '_')}|#{Time.now.utc.strftime("%H:%M, %B %d, %Y (UTC)")}}}", {:section => 'new'})
     end
   end
 end
