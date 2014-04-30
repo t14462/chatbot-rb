aef1293c1968671b2a89a92c978bc2fae1f09b1b
Sactage <sactage@gmail.com>
2014-04-30 18:27:49 -0400

    Update main.sample.rb

--- main.sample.rb
+++ main.sample.rb
@@ -3,5 +3,5 @@ require_relative './plugins/auto_tube' # Require all of the plugins we want here
 require_relative './plugins/holo'
 
 $bot = Chatbot::Client.new
-$bot.register_plugins(ChatBot::AutoTube, ChatBot::Holo) # Register the plugins with our bot
+$bot.register_plugins(Chatbot::AutoTube, Chatbot::Holo) # Register the plugins with our bot
 $bot.run! # Run the bot!
