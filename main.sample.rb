2ca3a11b8ec6650afb6218ca97d297cbcdbc2f51
Rinzler135 <Rinzler135@users.noreply.github.com>
2015-12-30 01:13:22 +0100

    Remove the auto_tube plugin from sample main.rb
    Since a special API key is now required, not initially loading the Youtube plugin may be a good idea

--- main.sample.rb
+++ main.sample.rb
@@ -1,7 +1,6 @@
 require_relative './client'
-require_relative './plugins/auto_tube' # Require all of the plugins we want here
 require_relative './plugins/admin' # You should generally always require this, as it includes commands like !quit/!plugins
 
 $bot = Chatbot::Client.new
-$bot.register_plugins(Chatbot::AutoTube, Chatbot::Admin) # Register the plugins with our bot
+$bot.register_plugins(Chatbot::Admin) # Register the plugins with our bot
 $bot.run! # Run the bot!
