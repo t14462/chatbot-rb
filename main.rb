e57e3c95c2c592d69b5061ca5c785e7d7406684b
t14462 <t14462@ya.ru>
2016-03-08 01:51:24 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
-require_relative './client'
-require_relative './plugins/admin'
-require_relative './plugins/wiki_log'
+require_relative 'client'
+require_relative 'plugins/admin'
+require_relative 'plugins/wiki_log'
 $bot = Chatbot::Client.new
 $bot.register_plugins(Chatbot::Admin, WikiLog)
 $bot.run!
