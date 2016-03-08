3d4a2372147cad1969b1aa3e0a0cc498d14ae512
t14462 <t14462@ya.ru>
2016-03-08 02:16:56 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 require_relative 'client'
+require_relative 'plugins/disk_log'
 require_relative 'plugins/admin'
 require_relative 'plugins/wiki_log'
+require_relative 'plugins/rutes'
 $bot = Chatbot::Client.new
-$bot.register_plugins(Chatbot::Admin, WikiLog)
+$bot.register_plugins(Chatbot::Admin, Chatbot::DiskLog, WikiLog, RUTES)
 $bot.run!
